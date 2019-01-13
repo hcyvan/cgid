494b774fa300825dc6bd83eb1ead35975eec810a
Navy Cheng <navych@126.com>
2019-01-13 14:34:23 +0800

    add gunicorn config

---
+++ config/gunicorn_config.tpl.py
@@ -0,0 +1,6 @@
+import multiprocessing
+
+bind = '0.0.0.0:8000'
+workers = multiprocessing.cpu_count() * 2 + 1
+accesslog = '/tmp/gunicorn_cgid_access.log'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
